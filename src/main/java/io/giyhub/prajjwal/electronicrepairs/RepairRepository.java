41dfcab6150928a62415b357c635db31a8c99555
prajjwal <prajjwal199275@gmail.com>
2023-06-07 11:16:55 +0530

    Implemented RepairRepository.java interface

---
+++ src/main/java/io/giyhub/prajjwal/electronicrepairs/RepairRepository.java
@@ -0,0 +1,6 @@
+package io.giyhub.prajjwal.electronicrepairs;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepairRepository extends JpaRepository<Repair, Long> {
+}
