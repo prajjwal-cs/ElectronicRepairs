482941a933b89939401ae4eff776663fc4f2d552
prajjwal <prajjwal199275@gmail.com>
2023-06-07 11:33:27 +0530

    added some methods RepairRepository.java interface

--- src/main/java/io/giyhub/prajjwal/electronicrepairs/RepairRepository.java
+++ src/main/java/io/giyhub/prajjwal/electronicrepairs/RepairRepository.java
@@ -1,6 +1,21 @@
 package io.giyhub.prajjwal.electronicrepairs;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RepairRepository extends JpaRepository<Repair, Long> {
+    List<Repair> findByCustomerName(String customerName);
+
+    List<Repair> findByStatus(String status);
+
+    List<Repair> findByDevice(String device);
+
+    List<Repair> findByCustomerNameAndDevice(String customerName, String device);
+
+    Long countByStatus(String status);
+
+    @Query("SELECT r FROM Repair r WHERE r.status = 'Pending' ORDER BY r.customerName ASC")
+    List<Repair> findPendingRepairsOrderedByCustomerName(String customerName);
 }
